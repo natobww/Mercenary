8863814804241c32e99c787cdd414c6993595985
natobww <natobww@163.com>
2017-03-07 00:24:53 +0800

    AS版本2.3有不同点,可以强制选择V1签名机制

---
+++ app/src/main/java/com/example/bgfvg/mercenary/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.bgfvg.mercenary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.natobww.mercenary.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
